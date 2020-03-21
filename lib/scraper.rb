e682bdb85fb51d16ed3472b44b4663c95dc96c5e
Joanne Qiong Sun <joannesun44@gmail.com>
2020-03-21 22:40:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,19 @@ require_relative './course.rb'
 
 class Scraper
   
+  def get_page
+    
+  end 
+  
+  def get_courses
+    
+  end 
+  
+  def make_courses
+    
+  end 
+  
+  
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,16 +28,6 @@ class Scraper
       end
     end
   end
-  
-  def get_page
-    
-  end 
-  
-  def get_courses
-    
-  end 
-  
-  def make_courses
   
   
 end
