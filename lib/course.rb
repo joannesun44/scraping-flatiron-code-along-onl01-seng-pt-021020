aa552d43fec09a3a39cce6c7775dedc88e9d53fc
Joanne Qiong Sun <joannesun44@gmail.com>
2020-03-21 21:28:57 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,7 +2,20 @@ class Course
   
   attr_accessor :title, :schedule, :description
   
-  All = []
+  @@all = []
+  
+  def initialize 
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.reset_all
+    self.all.clear
+  end 
+  
   
   
 end 
