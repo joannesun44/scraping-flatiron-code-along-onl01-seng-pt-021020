f46c24844201d3c5808b38fd43766425046b3cbf
Joanne Qiong Sun <joannesun44@gmail.com>
2020-03-21 23:34:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,9 @@ class Scraper
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
-  end 
+    end 
+  end
+ 
   
   
   def print_courses
